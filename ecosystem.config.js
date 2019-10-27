a7397d1452b30f3724f65e5762a32b2ed57154fb
m4v1 <marco.vivi@gmail.com>
2019-10-27 16:30:40 +0100

    removed pm2 cluster mode, it uses far more ram but performance are equal

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,6 @@ module.exports = {
       name: 'celebapi-express',
       script: 'dist/index.js',
       max_memory_restart: '200M',
-      instances: 4,
-      exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production'
       },
