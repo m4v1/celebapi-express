d0533cefc55cb9f9187a19a599a79058e9883f70
m4v1 <marco.vivi@gmail.com>
2019-10-27 16:22:08 +0100

    added a rertart policy if used memory reach 200mb, now the process starts in cluster mode with 4 instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  apps : [{
-    name: 'celebapi-express',
-    script: 'dist/index.js',
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    env: {
-      NODE_ENV: 'production'
-    },
-    env_production: {
-      NODE_ENV: 'production'
+  apps: [
+    {
+      name: 'celebapi-express',
+      script: 'dist/index.js',
+      max_memory_restart: '200M',
+      instances: 4,
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
     }
-  }],
+  ]
 };
