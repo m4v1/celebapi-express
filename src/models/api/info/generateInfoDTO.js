cd1bd24bfb7c57f7b03e6da520f09808bf1c3f2c
m4v1 <marco.vivi@gmail.com>
2019-10-28 13:29:21 +0100

    added a redis cache system to info route

--- src/models/api/info/generateInfoDTO.js
+++ src/models/api/info/generateInfoDTO.js
@@ -5,6 +5,8 @@ const generateDTO = (data, status) => {
   switch (status) {
     case 'db':
       return SuccessResponse.format(data, 'db', 200);
+    case 'cache':
+      return SuccessResponse.format(data, 'cache', 200);
     case 'remote':
       return SuccessResponse.format(data, 'remote', 200);
     case 'error':
--- src/routes/info.js
+++ src/routes/info.js
@@ -1,25 +1,44 @@
+import Redis from 'ioredis';
 import Database from '../services/db';
 import searchRemoteInfo from '../services/searchRemoteInfo';
 import generateDTO from '../models/api/info/generateInfoDTO';
 
 const getInfo = async (req, res) => {
   let status;
-  let profile = await Database.findByName(req.params.name);
-  status = 'db';
+  const cache = new Redis({
+    port: 6379, // Redis port
+    host: '127.0.0.1', // Redis host
+    db: 0
+  });
 
-  // if data is not on db search remotely
-  if (profile === undefined) {
-    profile = await searchRemoteInfo(req.params.name);
-    status = 'remote';
+  let profile = await cache.get(req.params.name);
+  profile = JSON.parse(profile);
+  console.log(profile);
+  status = 'cache';
 
-    // if remote search fails return error
+  if (!profile) {
+    profile = await Database.findByName(req.params.name);
+    status = 'db';
+
+    // if data is not on db search remotely
     if (profile === undefined) {
-      status = 'error';
+      profile = await searchRemoteInfo(req.params.name);
+      status = 'remote';
+
+      // if remote search fails return error
+      if (profile === undefined) {
+        status = 'error';
+      } else {
+        // save data to db, this should be sync
+        Database.insertProfile(profile);
+        cache.set(req.params.name, JSON.stringify(profile));
+      }
     } else {
-      // save data to db, this should be sync
-      Database.insertProfile(profile);
+      // profile is on the database but not in cache, so we cache it
+      cache.set(req.params.name, JSON.stringify(profile));
     }
   }
+
   const response = generateDTO(profile, status);
 
   return res.status(response.status).send(response);
