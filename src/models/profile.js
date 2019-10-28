279ac4444e1defe02caf561951378d0bf9455c4e
m4v1 <marco.vivi@gmail.com>
2019-10-28 16:45:52 +0100

    added jsonSchema to validate profiles on insert

--- src/models/profile.js
+++ src/models/profile.js
@@ -1,6 +1,4 @@
-import {
-  Model,
-} from 'objection';
+import { Model } from 'objection';
 import moment from 'moment';
 
 class Profile extends Model {
@@ -12,6 +10,19 @@ class Profile extends Model {
     const data = super.$parseDatabaseJson(json);
     data.bday = data.bday && moment(data.bday).format('Y-MM-DD');
     return data;
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['name', 'bday', 'bplace', 'bio'],
+      properties: {
+        name: { type: 'string', minLength: 1, maxLength: 150 },
+        bday: { type: 'date' },
+        bplace: { type: 'string', minLength: 1, maxLength: 150 },
+        bio: { type: 'string' }
+      }
+    };
   }
 }
 
