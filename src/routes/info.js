e13fec73992bccc39a46b487ad632b9c9b7c1dc9
m4v1 <marco.vivi@gmail.com>
2019-10-30 11:40:04 +0100

    added caching for 404 results to avoid high latency on them and preventing future searches on remote source during cache time frame

--- src/routes/info.js
+++ src/routes/info.js
@@ -28,6 +28,11 @@ const getInfo = async (req, res) => {
       // if remote search fails return error
       if (profile === undefined) {
         status = 'error';
+        /*
+         *  if profile can't be get from remote we set it to {} for 6h
+         *  this is to speed up future requests and bypass remote search
+         */
+        cache.set(req.params.name, JSON.stringify({}), 'EX', 21600);
       } else {
         // save data to db, this should be sync
         Database.insertProfile(profile);
@@ -38,6 +43,9 @@ const getInfo = async (req, res) => {
       // profile is on the database but not in cache, so we cache it for 1h
       cache.set(req.params.name, JSON.stringify(profile), 'EX', 3600);
     }
+  } else if (Object.entries(profile).length === 0) {
+    // profile is on the cache but it's an empty object, we return 404
+    status = 'error';
   }
 
   const response = generateDTO(profile, status);
