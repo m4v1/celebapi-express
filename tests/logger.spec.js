d276784ecdb769f0c5ab50bf49a2f21a54fcef1f
m4v1 <marco.vivi@gmail.com>
2019-11-14 11:10:02 +0100

    updated tests for logger

--- tests/logger.spec.js
+++ tests/logger.spec.js
@@ -1,6 +1,6 @@
 /* eslint-disable global-require */
 import pino from 'pino';
-import { Config } from '../src/config';
+import SentryLogger from '../src/services/sentry';
 
 beforeEach(() => {
   jest.resetModules();
@@ -9,39 +9,59 @@ beforeEach(() => {
 afterAll(() => {
   // after finishing reset NODE_ENV to testing to prevent errors on subsequent tests
   process.env.NODE_ENV = 'testing';
-  Config.load();
 });
 
 describe('Check logger instance on different environments', () => {
-  it('with env development logger should be pino', done => {
-    process.env.NODE_ENV = 'development';
-    Config.load();
-    const Logger = require('../src/services/logger').default;
-    const Pino = pino();
-    const serializedPino = JSON.stringify(Pino);
-    const serializedLogger = JSON.stringify(Logger.logger);
-    expect(serializedPino).toEqual(serializedLogger);
-    done();
-  });
-  it('with env testing logger should be pino', done => {
-    process.env.NODE_ENV = 'testing';
-    Config.load();
-    const Logger = require('../src/services/logger').default;
-    const Pino = pino();
-    const serializedPino = JSON.stringify(Pino);
-    const serializedLogger = JSON.stringify(Logger.logger);
-    expect(serializedPino).toEqual(serializedLogger);
-    done();
-  });
   it('with env production logger should be Sentry', done => {
     process.env.NODE_ENV = 'production';
-    Config.load();
+    process.env.USE_SENTRY = true;
+    process.env.SENTRY_DSN = 'https://66f3205d81094ddf9906a24851fed641@sentry.io/1793833';
     const Logger = require('../src/services/logger').default;
-    const SentryLogger = require('../src/services/sentry').default;
-    const Sentry = new SentryLogger();
-    const serializedSentry = JSON.stringify(Sentry);
-    const serializedLogger = JSON.stringify(Logger.logger);
-    expect(serializedSentry).toEqual(serializedLogger);
+    expect(Logger.logger.constructor.name).toBeTruthy();
+    expect(Logger.logger.constructor.name).toEqual(SentryLogger.constructor.name);
+    done();
+  });
+  it('with env development logger should be pino', done => {
+    process.env.NODE_ENV = 'development';
+    const Pino = pino();
+    const Logger = require('../src/services/logger').default;
+    expect(Logger.logger.constructor.name).toBeTruthy();
+    expect(Logger.logger.constructor.name).toEqual(Pino.constructor.name);
+    done();
+  });
+});
+
+describe('Check logger functions on different environments', () => {
+  it('testing logger methods with env production', done => {
+    process.env.NODE_ENV = 'production';
+    process.env.USE_SENTRY = true;
+    process.env.SENTRY_DSN = 'https://66f3205d81094ddf9906a24851fed641@sentry.io/1793833';
+    const Logger = require('../src/services/logger').default;
+    const LoggerInstance = Logger.getLogger();
+    LoggerInstance.info = jest.fn();
+    Logger.info('foo');
+    LoggerInstance.warn = jest.fn();
+    Logger.warn('foo');
+    LoggerInstance.error = jest.fn();
+    Logger.error('foo');
+    expect(LoggerInstance.info).toHaveBeenCalledTimes(1);
+    expect(LoggerInstance.warn).toHaveBeenCalledTimes(1);
+    expect(LoggerInstance.error).toHaveBeenCalledTimes(1);
+    done();
+  });
+  it('testing logger methods with env development', done => {
+    process.env.NODE_ENV = 'development';
+    const Logger = require('../src/services/logger').default;
+    const LoggerInstance = Logger.getLogger();
+    LoggerInstance.info = jest.fn();
+    Logger.info('foo');
+    LoggerInstance.warn = jest.fn();
+    Logger.warn('foo');
+    LoggerInstance.error = jest.fn();
+    Logger.error('foo');
+    expect(LoggerInstance.info).toHaveBeenCalledTimes(1);
+    expect(LoggerInstance.warn).toHaveBeenCalledTimes(1);
+    expect(LoggerInstance.error).toHaveBeenCalledTimes(1);
     done();
   });
 });
