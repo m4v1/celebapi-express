87466ce774cce30b8b119ab724b3336bf2ffc6b9
m4v1 <marco.vivi@gmail.com>
2020-06-11 10:43:58 +0200

    speculative fix to avoid being spammed by youtube 403 errors after api quota depletion

--- src/services/searchRemoteVideos.js
+++ src/services/searchRemoteVideos.js
@@ -1,14 +1,19 @@
 import tubeSearch from 'youtube-search';
 import logger from './logger';
 
-const searchRemoteVideos = async name => {
+const manageVideoError = () => {
+  // nullify errors from api depleted quota
+  return null;
+};
+
+const searchRemoteVideos = async (name) => {
   try {
     const opts = {
       maxResults: 6,
       type: 'video',
-      key: process.env.YOUTUBE_API_KEY
+      key: process.env.YOUTUBE_API_KEY,
     };
-    const res = await tubeSearch(name, opts);
+    const res = await tubeSearch(name, opts, manageVideoError);
     return res;
   } catch (error) {
     logger.error(error);
