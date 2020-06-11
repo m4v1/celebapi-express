df2117d07bd03dff97cc00836b8e6a18038155b1
m4v1 <marco.vivi@gmail.com>
2020-06-11 11:12:21 +0200

    Revert "speculative fix to avoid being spammed by youtube 403 errors after api quota depletion"
    This reverts commit 87466ce774cce30b8b119ab724b3336bf2ffc6b9.

--- src/services/searchRemoteVideos.js
+++ src/services/searchRemoteVideos.js
@@ -1,19 +1,14 @@
 import tubeSearch from 'youtube-search';
 import logger from './logger';
 
-const manageVideoError = () => {
-  // nullify errors from api depleted quota
-  return null;
-};
-
-const searchRemoteVideos = async (name) => {
+const searchRemoteVideos = async name => {
   try {
     const opts = {
       maxResults: 6,
       type: 'video',
-      key: process.env.YOUTUBE_API_KEY,
+      key: process.env.YOUTUBE_API_KEY
     };
-    const res = await tubeSearch(name, opts, manageVideoError);
+    const res = await tubeSearch(name, opts);
     return res;
   } catch (error) {
     logger.error(error);
